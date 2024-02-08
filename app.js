23b195067f149c6f37a07d30c4c96fab9c7ff3fc
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-02-08 14:05:30 +0000

    stuff added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express'); //calls the express libary
-
+const morgan = require('morgan');
 const app = express(); // changes the name of the express call to app, saves time on typing
 
 app.set('view engine', 'ejs'); // this calls for the ejs libary
@@ -10,13 +10,7 @@ app.listen(3000); //sets the port number to 3000
 //middle ware to access static files
 app.use(express.static('css'));
 
-app.use((req, res, next) => {
-    console.log('new request has been made');
-    console.log('Host name: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next();
-}); 
+app.use(morgan('dev'));
 
 
  app.get('/', (req, res) => { //this gets the request from the navigation from the webpage and loads that page
