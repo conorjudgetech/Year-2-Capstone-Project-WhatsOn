3fcd0cde991a7409e7acc28397a11bd4cf510018
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-03-07 13:34:48 +0000

    removing lines from app.js that linked the website to other pages that no longer exist

--- app.js
+++ app.js
@@ -17,22 +17,6 @@ app.use(morgan('dev'));
     res.render('index', {title: 'Home'}); // tells the code to render the index file
 });
 
-app.get('/follow', (req, res) => { //this gets the request from the navigation from the webpage and loads that page
-    res.render('follow', {title: 'Follow Page'}); // tells the code to render the index file
-});
-
-app.get('/signup', (req, res) => { //this gets the request from the navigation from the webpage and loads that page
-    res.render('signup', {title: 'Sign Up'}); // tells the code to render the index file
-});
-
-app.get('/login', (req, res) => { //this gets the request from the navigation from the webpage and loads that page
-    res.render('login', {title: 'Login'}); // tells the code to render the index file
-});
-
-app.get('/contact-us', (req, res) => { //this gets the request from the navigation from the webpage and loads that page
-    res.render('contact-us', {title: 'Conact Us'}); // tells the code to render the index file
-});
-
 
 app.use((req, res) => { //this is used to direct the user to the 404 page if the page they are looking for does not exist 
     res.status(404).render('404', {title: '404 Page'});
