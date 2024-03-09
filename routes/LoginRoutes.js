e9c942af5c45532d6ebcaaa1a9d329070cd6f378
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-03-09 15:59:56 +0000

    I have removed these files because I found a more simple format to write the code!

--- routes/LoginRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const loginController = require('../controllers/LoginController');
-
-const router = express.Router();
-
-router.get('/:id', loginController.signUp);
-router.get('/:id', loginController.deleteAccount);
