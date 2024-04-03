87ed05563181343ff5ea9af6e7d4074a71011251
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-04-03 14:23:10 +0100

    Testing more ways of meetup api

--- app.js
+++ app.js
@@ -44,18 +44,13 @@ const startApp = () => {
 
 //this method should allow us to avoid the cors error 
 app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-type", "Authorization");
-    if(req.method === "OPTIONS")
-    {
-      return res.sendStatus(200);
-    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
     next();
 });
+
+app.get('https://api.meetup.com/events');
 
 startApp();
 
