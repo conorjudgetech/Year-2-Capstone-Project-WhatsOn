0de1c9c93ddf73c7139b3008abea8f97706253f2
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-04-08 15:14:12 +0100

    this is my current progress with trying to the load the api. I have went with trying to grab a token but currently i am unable to load any sort of token in this current moment in time. I am getting undefined so I believe I am on some sort of right path as regards to loading the api. I need more testing and learning before I can get the api to work. The technologies introduced to make this happen are axios and .env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 import morgan from 'morgan';
-
+import {config} from 'dotenv';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,34 +11,51 @@ app.set('view engine', 'ejs'); // this calls for the ejs libary
 app.use(express.static('css')); //loads all of the static files from the css folder
 
 app.use(morgan('dev')); //enables logging information regarding the server
-const endpoint = 'https://api.meetup.com/gql'
-app.use(express.json());
-app.use(cors());
+
+config();
+const {CLIENT_ID, APP_SECRET} = process.env;
+
+const base ="https://api.meetup.com/gql";
 
 
-const query = `
-  query{
-    self{
-      id,
-      name
-    }
-  }
-`;
 
-const body = JSON.stringify({query});
+//test route 
+app.get("/test", async (req,res) => {
+  const data = await generateAccessToken();
+  console.log(data);
 
-const request = axios({
-  url:endpoint,
-  method: "GET",
-  mode: "cors",
-  headers: {
-    'Authorization': '{cvdgj137jq4nejecgnh6ce0chr}',
-    "Content-Type": "application/json"
-  },
-  data: body
+  res.json(data);
 });
 
-request.then(({data}) => console.log(data));
+
+function testQuery(test) {
+  return queryFetch(`
+      query {
+          self {
+              id,
+              name
+          }
+      }
+  `, {self: test}).then(data => {
+      console.log(data.data.event);
+    });
+  }
+
+async function generateAccessToken()
+{
+  const reposne = await axios({
+    url:"https://secure.meetup.com/oauth2/authorize",
+    method: "post",
+    data:"grant_type=client_credentials",
+    auth: {
+      username: CLIENT_ID,
+      password: APP_SECRET,
+    },
+  });
+  reposne.data;
+}
+
+
 
 
 app.listen(port, () =>{
