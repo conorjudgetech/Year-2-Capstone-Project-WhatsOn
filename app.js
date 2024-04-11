c91bd7f63a5716eb0f6cbcfcdd4ae03fb30ee2bb
Eoin Fitzsimons <eoin00fitzsimons@gmail.com>
2024-04-11 18:48:34 +0100

    Revert "Update app.js"
    This reverts commit 53ca894c7acdf56a447b4330e6f73a2bbe25df92.

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-import express from "express";
+import express from 'express';
 // import { ApolloServer, gql } from 'apollo-server-express';
-import morgan from "morgan";
+import morgan from 'morgan';
 // import fetch from 'node-fetch';
 
-import fetchData from "./Data Fetching/fetchData.js";
+import fetchData from './Data Fetching/fetchData.js';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs"); // this calls for the ejs libary
-app.use(express.static("css")); //loads all of the static files from the css folder
+app.set('view engine', 'ejs'); // this calls for the ejs libary
+app.use(express.static('css')); //loads all of the static files from the css folder
 
-app.use(morgan("dev")); //enables logging information regarding the server
+app.use(morgan('dev')); //enables logging information regarding the server
+
+
+// Call fetchData when your server starts fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+fetchData().then(() => {
+  console.log('Data fetched successfully');
+}).catch((error) => {
+  console.error(`Error fetching data: ${error}`);
+});
 
 const startApp = () => {
-  // Call fetchData when your server starts fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-  fetchData()
-    .then(() => {
-      console.log("Data fetched successfully");
-    })
-    .catch((error) => {
-      console.error(`Error fetching data: ${error}`);
-    });
+  //inject apollo server on express app
+
   // server.applyMiddleware({ app });
   app.listen(port, () => console.log(`Server is running on port ${port}`));
-};
+}
 
 startApp();
 
-app.use((req, res) => {
-  //this is used to direct the user to the 404 page if the page they are looking for does not exist
-  res.status(404).render("404", { title: "404 Page" });
+app.get('/', (req, res) => { //this gets the request from the navigation from the webpage and loads that page
+  res.render('index', { title: 'Home' }); // tells the code to render the index file
+});
+
+app.use((req, res) => { //this is used to direct the user to the 404 page if the page they are looking for does not exist 
+  res.status(404).render('404', { title: '404 Page' });
 });
